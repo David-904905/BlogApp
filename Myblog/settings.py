092b342f49941558575bd765f4111b90485e9082
David-904905 <notyouraverageprespective@gmail.com>
2024-09-24 15:21:09 +0100

    Update settings.py

--- Myblog/settings.py
+++ Myblog/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'davdevtestmail@gmail.com'
-EMAIL_HOST_PASSWORD = 'bjnf ctvv yftt xlot'
+EMAIL_HOST_USER = 'testemail'
+EMAIL_HOST_PASSWORD = 'testpassword'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
